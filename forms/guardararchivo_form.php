6ec63f77cbe47ef97cc8a6b841d4df6b5acdfac4
danialves21 <dannuu_bkm@hotmail.com>
2016-12-21 10:05:17 -0300

    Changes made on index.php and guardararchivo form is finished

--- forms/guardararchivo_form.php
+++ forms/guardararchivo_form.php
@@ -1,20 +1,43 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
-require_once($CFG->libdir . "/formslib.php");
+require_once($CFG->libdir . "/formslib.php"); //lineas 2 a 4 siempre van!
 
 class guardararchivo_subirarchivo_form extends moodleform {
 	public function definition() {
 		global $DB, $CFG;
 		$mform = $this->_form; // Siempre va!!!
 		
+		//addElement('tipo de elemento', 'nombre elemento', 'etiqueta elemento')
+		//text
 		$mform->addElement('text', 'filename', "Nombre del archivo");
-		$mform->addElement('filepicker', 'userfile', "Subir el archivo");
+		$mform->setType('filename', PARAM_TEXT);
+		//regla para cantidad minima de caracteres en el filename
+		//$mform->addRule('filename', "Por favor ingrese un nombre de mínimo 4 caracteres", 'minlength', 4, 'client');
+		//$mform->addRule('filename', "Por favor ingrese un nombre", 'required', null, 'client');
 		
+		//filepicker
+		$mform->addElement('filepicker', 'userfile', "Subir el archivo");
+		$mform->setType('userfile', PARAM_FILE);
+		$mform->addRule('userfile', "Por favor suba un archivo antes de continuar", 'required', null, 'client');		
+			
+		//buttons
 		$this->add_action_buttons(true, 'Subir'); //Siempre con this!
 	}
 	
-	public function validation(){
+	public function validation($data, $files){
+		$errors = array();
 		
+		$filename = $data['filename'];
+		$userfile = $data['userfile'];
+		
+		if(empty($filename)) {
+			$errors['filename'] = "Por favor ingrese un nombre";
+		}
+		
+		if(empty($userfile)) {
+			$errors['userfile'] = "Por favor suba un archivo antes de continuar";
+		}
+		return $errors;
 	}
 }
--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 // Funciones
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->dirroot . "/local/guardararchivo/forms/guardararchivo_form.php");
-global $PAGE, $CFG, $OUTPUT, $DB;
+global $PAGE, $CFG, $OUTPUT, $DB, $USER;
 require_login();
+if (isguestuser()) {
+	die();
+}
 
 // Construcción de la pagina en formato moodle (siempre al inicio)
 $url = new moodle_url('/local/guardararchivo/index.php');
@@ -14,10 +17,63 @@ $PAGE->set_pagelayout('standard');
 $title = "Subir un archivo";
 $PAGE->set_title($title);
 $PAGE->set_heading($title);
+
 echo $OUTPUT->header();
+$action = optional_param('action','viewfiles',PARAM_TEXT);
 
 $mform = new guardararchivo_subirarchivo_form();
-$mform->display();
+
+if ($action == 'addfiles') {
+	if ($mform->is_cancelled()) { //si se presiona boton cancelar
+		$home = new moodle_url('/my/');
+		redirect($home);
+	} else if ($mform->get_data()) { //se procesan datos validados
+		require_capability('local/guardararchivo:upload', $context);
+	
+		$data = $mform->get_data();
+	
+		$path = $CFG->dataroot. "/temp/local/guardararchivo";
+		if(!file_exists($path . "/unread/")) {
+			mkdir($path . "/unread/", 0777, true);
+		}
+		//Guardar archivo
+		$filename = $mform->get_new_filename('userfile');
+		$file = $mform->save_file('userfile', $path."/unread/".$filename,false);
+		$time = strtotime(date("d-m-Y H:s:i"));
+	
+		//validar que se subió bien el archivo
+		$uploadfile = $path . "/unread/".$data->filename;
+		
+		//Datos del archivo
+		$file_record = array(
+						'contextid' => $context->id,
+						'component' => 'local_guardararchivo',
+						'filearea' => 'draft',
+						'itemid' => 0,
+						'filepath' => '/',
+						'filename' => $data->filename,
+						'timecreated' => time(),
+						'timemodified' => time(),
+						'userid' => $USER->id,
+						'author' => $USER->firstname." ".$USER->lastname,
+						'license' => 'allrightsreserved'
+		);
+				
+		//Información del nuevo archivo
+		$fileinfo = $fs->create_file_from_pathname($file_record, $uploadfile);
+	
+		//Se cambia valor al action
+		$action = 'viewfiles';
+	}
+}
+
+//Muestra el formulario
+if ($action == 'addfiles') {
+	$mform->display();
+} else if ($action == 'viewfiles') {
+	echo "hola moodle";
+	$this->add_action_buttons(null,'Subir nuevo archivo');
+}
 
 //Siempre al final
 echo $OUTPUT->footer();
